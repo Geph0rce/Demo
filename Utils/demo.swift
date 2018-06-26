633303106f4eb49a98574b4121e1688571647eb7
Roger <roger@RogerdeMacBook-Pro.local>
2018-06-27 07:35:42 +0800

    refine wwdc, add swift

---
+++ Utils/demo.swift
@@ -0,0 +1,48 @@
+//
+//  demo.swift
+//  demo
+//
+//  Created by Roger on 2018/6/27.
+//  Copyright © 2018 Zen. All rights reserved.
+//
+
+import Foundation
+
+struct Listing : Codable {
+    var id : Int
+}
+
+class DecodeDemo {
+
+    var json:Data = Data();
+    
+    
+    
+    
+    
+    
+    
+    func decodeData() throws {
+        let decoder = JSONDecoder()
+        let listing = try decoder.decode(Listing.self, from:json)
+
+        print(listing);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
