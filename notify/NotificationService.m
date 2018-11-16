1179d62d074e49caa518894a1dcc351e394451e7
qianjie <qianjie@58ganji.com>
2018-11-16 14:21:17 +0800

    Notification Extendsion

--- notify/NotificationService.m
+++ notify/NotificationService.m
@@ -1,19 +1,17 @@
 //
 //  NotificationService.m
-//  notify
+//  notification
 //
-//  Created by qianjie on 2018/2/26.
-//  Copyright © 2018年 Zen. All rights reserved.
+//  Created by qianjie on 2018/3/28.
+//  Copyright © 2018年 anjuke inc. All rights reserved.
 //
 
 #import "NotificationService.h"
-#import <CoreLocation/CoreLocation.h>
 
-@interface NotificationService () <CLLocationManagerDelegate>
+@interface NotificationService ()
 
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
 @property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
-@property (nonatomic, strong) CLLocationManager *manager;
 
 @end
 
@@ -22,50 +20,43 @@
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
-    [self.manager requestAlwaysAuthorization];
-    [self.manager startUpdatingLocation];
+    
+    //1. 下载
+    NSDictionary *dict =  self.bestAttemptContent.userInfo;
+    NSDictionary *apsDict = dict[@"aps"];
+    NSString *urlString = apsDict[@"alert"][@"image-url"];
+    NSLog(@"url: %@", urlString);
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (!error) {
+            NSString *pathExtension = url.absoluteString.pathExtension;
+            NSString *path = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject
+                               stringByAppendingPathComponent:@"image"] stringByAppendingPathExtension:pathExtension];
+            //2. 保存数据
+            [data writeToFile:path atomically:NO];
+            
+            //3. 添加附件
+            UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"attachment" URL:[NSURL fileURLWithPath:path] options:nil error:nil];
+            if (attachment) {
+                self.bestAttemptContent.attachments = @[attachment];
+            }
+        }
+        
+        //4. 返回新的通知内容
+        self.contentHandler(self.bestAttemptContent);
+    }];
+    
+    [task resume];
 }
 
 
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-   // self.contentHandler(self.bestAttemptContent);
-}
-
-
-#pragma mark - CLLocationManagerDelegate
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
-    NSMutableString *coordinate = [NSMutableString string];
-    for (CLLocation *location in locations) {
-        [coordinate appendFormat:@"lng: %@ lat: %@", @(location.coordinate.longitude), @(location.coordinate.latitude)];
-    }
-    NSString *url = [NSString stringWithFormat:@"https://www.baidu.com/location/coordinate=%@", coordinate];
-    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-    NSURLSessionDataTask *task = [session dataTaskWithURL:[NSURL URLWithString:url] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (!error) {
-            
-        }
-    }];
-    [task resume];
-    self.bestAttemptContent.title = coordinate;
     self.contentHandler(self.bestAttemptContent);
 }
 
-
-#pragma mark - Getters
-
-- (CLLocationManager *)manager {
-    if (!_manager) {
-        _manager = [[CLLocationManager alloc] init];
-        _manager.delegate = self;
-        _manager.allowsBackgroundLocationUpdates = YES;
-        _manager.pausesLocationUpdatesAutomatically = NO;
-        _manager.distanceFilter = kCLDistanceFilterNone;
-    }
-    return _manager;
-}
-
 @end
+
